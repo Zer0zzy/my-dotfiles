5d7614274bc0bdf94fbfd47a8d5753690e67b4c3
Zer0zzy <113624363+Zer0zzy@users.noreply.github.com>
2024-12-28 08:26:32 -0500

    update komorebi config folder.

--- install.ps1
+++ install.ps1
@@ -12,16 +12,28 @@ If (-Not (Get-Command scoop -ErrorAction SilentlyContinue)) {
   Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
   resetEnv Path
   resetEnv AppPath
+} Else {
   scoop install 7zip
   scoop install git
   scoop install gpg
   scoop install bitwarden-cli
-}
-
-If (-Not (Get-Command chezmoi -ErrorAction SilentlyContinue)) {
-  scoop install chezmoi
   resetEnv Path
   resetEnv AppPath
 }
 
-chezmoi init --apply "--source=$PSScriptRoot"
+If (Get-Command bw -ErrorAction SilentlyContinue) {
+  bw login
+  $BW_SESSION_INIT=$(bw unlock --raw)
+  [System.Environment]::SetEnvironmentVariable('BW_SESSION',"$BW_SESSION_INIT")
+
+  If (-Not (Get-Command chezmoi -ErrorAction SilentlyContinue)) {
+    scoop install chezmoi
+    resetEnv Path
+    resetEnv AppPath
+  }
+
+  chezmoi init --apply "--source=$PSScriptRoot"
+
+} Else {
+  Write-Output "Bitwarden is not installed. Cannot proceed with chezmoi apply."
+}
