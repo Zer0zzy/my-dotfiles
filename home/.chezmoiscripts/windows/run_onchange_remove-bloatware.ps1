fd0d32de41747e7ab90b16ccc3108586ff570e34
Zer0zzy <113624363+Zer0zzy@users.noreply.github.com>
2024-12-31 09:35:21 -0500

    Update run_onchange_remove-bloatware.ps1

--- home/.chezmoiscripts/windows/run_onchange_remove-bloatware.ps1
+++ home/.chezmoiscripts/windows/run_onchange_remove-bloatware.ps1
@@ -1,73 +1,102 @@
+# Script can only run as Admin
 # To run this script:
-# 1. Run Windows PowerShell as Administrator
-# 2. Run `Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process`
-# 3. Run `chezmoi apply ~/.chezmoiscripts/windows/remove-bloat.ps1`
+# 1. Run `Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process`
+# 1. Run `chezmoi apply ~/.chezmoiscripts/windows/remove-bloatware.ps1`
 #
 # automate running this script as Administrator
-#Requires -RunAsAdministrator
+# Check if Powershell is running as Admin
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+#"No Administrative rights, it will display a popup window asking user for Admin rights"
+    $arguments = "& '" + $myinvocation.mycommand.definition + "'"
+    Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $arguments
+    break
+}
+#"After user clicked Yes on the popup, your file will be reopened with Admin rights"
+#"Put your code here"
+$AppXErrors = $False;
 
-# FIXME remove CoPilot
 # FIXME remove Spotify
 # FIXME remove LinkedIn
 # FIXME remove Windows 11 bottom left widget
-Get-AppxPackage -AllUsers Clipchamp.Clipchamp | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.549981C3F5F10 | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.BingNews | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.BingWeather | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.GamingApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.GetHelp | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.Getstarted | Remove-AppPackage
-Get-AppxPackage -AllUsers Microsoft.Microsoft3DViewer | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.MicrosoftOfficeHub | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.MicrosoftSolitaireCollection | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.MicrosoftStickyNotes | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.MixedReality.Portal | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.MSPaint | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.OutlookForWindows | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.Paint | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.People | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.ScreenSketch | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.SkypeApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.StorePurchaseApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.Windows.Photos | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.WindowsAlarms | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.WindowsCalculator | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.WindowsCamera | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.WindowsCommunicationsApps | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.WindowsFeedbackHub | Remove-AppPackage
-Get-AppxPackage -AllUsers Microsoft.WindowsMaps | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.WindowsSoundRecorder | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.WindowsStore | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.Xbox.TCUI | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.XboxApp | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.XboxGameOverlay | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.XboxGamingOverlay | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.XboxIdentityProvider | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.YourPhone | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.ZuneMusic | Remove-AppxPackage
-Get-AppxPackage -AllUsers Microsoft.ZuneVideo | Remove-AppxPackage
+$Packages = @(
+Microsoft.Copilot, 
+ClipChamp.ClipChamp,
+DellInc.DellSupportAssistforPCs,
+DellInc.DellDigitalDelivery,
+DellInc.DellOptimizer,
+Microsoft.549981C3F5F10,
+Microsoft.BingNews,
+Microsoft.BingWeather,
+Microsoft.GamingApp,
+Microsoft.GetHelp,
+Microsoft.GetStarted,
+Microsoft.Microsoft3DViewer,
+# Microsoft.MicrosoftOfficeHub,
+Microsoft.MicrosoftSolitaireCollection,
+Microsoft.MicrosoftStickyNotes,
+Microsoft.MixedReality.Portal,
+# Microsoft.MSPaint,
+# Microsoft.OutlookForWindows,
+# Microsoft.Paint,
+Microsoft.People,
+Microsoft.ScreenSketch,
+Microsoft.SkypeApp,
+Microsoft.StorePurchaseApp, # Marks Purchases in Msft Store
+Microsoft.Todos,
+Microsoft.Windows.ParentalControls,
+# Microsoft.Windows.Photos,
+Microsoft.WindowsAlarms,
+Microsoft.WindowsCalculator,
+Microsoft.WindowsCamera,
+# Microsoft.WindowsCommunicationsApps, # Start Menu Apps for People/Mail/Calendar
+Microsoft.WindowsFeedbackHub,
+Microsoft.WindowsMaps,
+# Microsoft.WindowsSoundRecorder,
+# Microsoft.WindowsStore,
+Microsoft.Xbox.TCUI,
+Microsoft.XboxApp,
+Microsoft.XboxGameOverlay,
+Microsoft.XboxGamingOverlay,
+Microsoft.XboxIdentityOverlay,
+Microsoft.XboxSpeechToTextOverlay,
+# Microsoft.YourPhone,
+Microsoft.ZuneMusic,
+Microsoft.ZuneVideo,
+MicrosoftWindows.LKG.DesktopSpotlight
+);
+
+$Packages | ForEach-Object {
+    $Package = $_
+    If (Get-AppxPackage -AllUsers $Package | Remove-AppxPackage -ErrorAction SilentlyContinue) {
+        Write-Output "Removed $Package"
+    } Else {
+        Write-Error "Could not remove $Package."
+        $AppxErrors = $True;
+    }
+}
+If ($AppxErrors) {Get-AppxLog}
 
 # https://learn.microsoft.com/en-us/answers/questions/1421927/uninstall-unpin-spotify-whatsapp-etc-using-script
 
-$Junk = "xbox|phone|disney|skype|spotify|groove|solitaire|zune|mixedreality|tiktok|adobe|prime|soundrecorder|bingweather!3dviewer"
-"Removing apps for this user only."
-$packages = Get-AppxPackage | Where-Object {  $_.Name -match $Junk  } | Where-Object -Property NonRemovable -eq $false 
-foreach ($appx in $packages) {
-    "Removing {0}" -f $appx.name
-    Remove-AppxPackage $appx   
-}
-""
-"Removing apps for all users."
-$packages = Get-AppxPackage -AllUsers | Where-Object {  $_.Name -match $Junk  } | Where-Object -Property NonRemovable -eq $false 
-foreach ($appx in $packages) {
-    "Removing {0}" -f $appx.name
-    Remove-AppxPackage $appx -AllUsers 
-}
-""
-"Removing provisioned apps."
-$packages = Get-AppxProvisionedPackage -Online | Where-Object {  $_.DisplayName -match $Junk  }
-foreach ($appx in $packages) {
-    "Removing {0}" -f $appx.displayname
-    Remove-AppxProvisionedPackage -online -allusers -PackageName $appx.PackageName 
-}
+#$Junk = "xbox|phone|disney|skype|spotify|groove|solitaire|zune|mixedreality|tiktok|adobe|prime|soundrecorder|bingweather!3dviewer"
+# "Removing apps for this user only."
+# $packages = Get-AppxPackage | Where-Object {  $_.Name -match $Junk  } | Where-Object -Property NonRemovable -eq $false 
+# foreach ($appx in $packages) {
+#     "Removing {0}" -f $appx.name
+#     Remove-AppxPackage $appx -ErrorAction SilentlyContinue
+# }
+# ""
+# "Removing apps for all users."
+# $packages = Get-AppxPackage -AllUsers | Where-Object {  $_.Name -match $Junk  } | Where-Object -Property NonRemovable -eq $false 
+# foreach ($appx in $packages) {
+#     "Removing {0}" -f $appx.name
+#     Remove-AppxPackage $appx -AllUsers -ErrorAction SilentlyContinue
+# }
+# ""
+# "Removing provisioned apps."
+# $packages = Get-AppxProvisionedPackage -Online | Where-Object {  $_.DisplayName -match $Junk  }
+# foreach ($appx in $packages) {
+#     "Removing {0}" -f $appx.displayname
+#     Remove-AppxProvisionedPackage -online -allusers -PackageName $appx.PackageName -ErrorAction SilentlyContinue
+# }
